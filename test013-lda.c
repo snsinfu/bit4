4c5d32446d01e99b46d030b78356fd72f8141c0b
frickiericker <frickiericker@gmail.com>
2017-06-14 03:39:32 +0900

    test013: Reorganize code

--- test013-lda.c
+++ test013-lda.c
@@ -2,8 +2,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Print matrix to stdout in tabular form.
+void print_matrix(
+    int rows, int cols, const double matrix[rows][cols]
+);
+
+// Fit LDA to given word frequency data and return latent distributions.
 void latent_dirichlet_allocation(
-    int n_docs, int n_words, int n_topics, const double word_freq[n_docs][n_words]
+    int n_docs, int n_words, int n_topics,
+    const double word_freq[n_docs][n_words],
+    double doc_topic_distr[n_docs][n_topics],
+    double topic_word_distr[n_topics][n_words]
 );
 
 int main(void) {
@@ -21,71 +30,23 @@ int main(void) {
         {10, 3, 4, 90, 5},
     };
 
-    latent_dirichlet_allocation(n_docs, n_words, n_topics, word_freq);
+    double doc_topic_distr[n_docs][n_topics];
+    double topic_word_distr[n_topics][n_words];
+
+    latent_dirichlet_allocation(
+        n_docs, n_words, n_topics,
+        word_freq, doc_topic_distr, topic_word_distr
+    );
+
+    puts("Topic-word distribution:");
+    print_matrix(n_topics, n_words, topic_word_distr);
+    puts("");
+    puts("Document-topic distribution:");
+    print_matrix(n_docs, n_topics, doc_topic_distr);
 
     return 0;
 }
 
-// Compute approximate digamma function.
-double digamma(double x) {
-    if (x < 1e-6) {
-        const double EULER = 0.5772156649015328606;
-        return -EULER - 1 / x;
-    }
-
-    double result = 0;
-
-    // psi(x + 1) = psi(x) + 1/x
-    while (x < 6) {
-        result -= 1 / x;
-        x += 1;
-    }
-
-    // psi(x) = log(x) - 1/(2x) - 1/(12x^2) + 1/(120x^4) - 1/(252x^6) + ...
-    double r = 1 / x;
-    result += log(x) - 5 * r;
-    r *= r;
-    result -= r * (1./12 - r * (1./120 - r * (1./252)));
-
-    return result;
-}
-
-// Randomize matrix elements with numbers in [lo,hi].
-void randomize_matrix(int rows, int cols, double matrix[rows][cols], double lo, double hi) {
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            matrix[i][j] = lo + (hi - lo) * rand() / RAND_MAX;
-        }
-    }
-}
-
-// Compute E[log X] for Dirichlet variable X.
-void exp_dirichlet_expect(int n, const double distr[n], double result[n]) {
-    double sum = 0;
-    for (int i = 0; i < n; i++) {
-        sum += distr[i];
-    }
-
-    const double digamma_sum = digamma(sum);
-    for (int i = 0; i < n; i++) {
-        result[i] = exp(digamma(distr[i]) - digamma_sum);
-    }
-}
-
-// Normalize probability distribution.
-void normalize_distribution(int n, double distr[n]) {
-    double sum = 0;
-    for (int i = 0; i < n; i++) {
-        sum += distr[i];
-    }
-
-    const double norm = 1 / sum;
-    for (int i = 0; i < n; i++) {
-        distr[i] *= norm;
-    }
-}
-
-// Print matrix to stdout in tabular form.
 void print_matrix(int rows, int cols, const double matrix[rows][cols]) {
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
@@ -94,9 +55,31 @@ void print_matrix(int rows, int cols, const double matrix[rows][cols]) {
     }
 }
 
-// Fit LDA to given corpus and print latent distributions.
+// Compute approximate digamma function.
+static double digamma(
+    double x
+);
+
+// Randomize matrix elements with numbers in [lo,hi].
+static void randomize_matrix(
+    int rows, int cols, double matrix[rows][cols], double lo, double hi
+);
+
+// Compute E[log X] for Dirichlet variable X.
+static void exp_dirichlet_expect(
+    int n, const double distr[n], double result[n]
+);
+
+// Normalize probability distribution.
+static void normalize_distribution(
+    int n, double distr[n]
+);
+
 void latent_dirichlet_allocation(
-    int n_docs, int n_words, int n_topics, const double word_freq[n_docs][n_words]
+    int n_docs, int n_words, int n_topics,
+    const double word_freq[n_docs][n_words],
+    double doc_topic_distr[n_docs][n_topics],
+    double topic_word_distr[n_topics][n_words]
 ) {
     // Heuristic priors from sklearn
     const double doc_topic_prior = 1. / n_topics;
@@ -106,15 +89,12 @@ void latent_dirichlet_allocation(
     const int max_em_iter = 100;
     const int max_e_iter = 100;
 
-    // Latent variables
-    double topic_word_distr[n_topics][n_words];
-    double doc_topic_distr[n_docs][n_topics];
-    double phi[n_docs][n_words][n_topics];
-
     // Start EM iterations
     randomize_matrix(n_topics, n_words, topic_word_distr, 0.9, 1.1);
 
     for (int em_iter = 0; em_iter < max_em_iter; em_iter++) {
+
+        double phi[n_docs][n_words][n_topics];
 
         // E step: Estimate document-topic distribution and phi
         randomize_matrix(n_docs, n_topics, doc_topic_distr, 0.9, 1.1);
@@ -177,10 +157,59 @@ void latent_dirichlet_allocation(
     for (int k = 0; k < n_topics; k++) {
         normalize_distribution(n_words, topic_word_distr[k]);
     }
+}
 
-    puts("Topic-word distribution:");
-    print_matrix(n_topics, n_words, topic_word_distr);
-    puts("");
-    puts("Document-topic distribution:");
-    print_matrix(n_docs, n_topics, doc_topic_distr);
+double digamma(double x) {
+    if (x < 1e-6) {
+        const double EULER = 0.5772156649015328606;
+        return -EULER - 1 / x;
+    }
+
+    double result = 0;
+
+    // psi(x + 1) = psi(x) + 1/x
+    while (x < 6) {
+        result -= 1 / x;
+        x += 1;
+    }
+
+    // psi(x) = log(x) - 1/(2x) - 1/(12x^2) + 1/(120x^4) - 1/(252x^6) + ...
+    double r = 1 / x;
+    result += log(x) - 5 * r;
+    r *= r;
+    result -= r * (1./12 - r * (1./120 - r * (1./252)));
+
+    return result;
+}
+
+void randomize_matrix(int rows, int cols, double matrix[rows][cols], double lo, double hi) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            matrix[i][j] = lo + (hi - lo) * rand() / RAND_MAX;
+        }
+    }
+}
+
+void exp_dirichlet_expect(int n, const double distr[n], double result[n]) {
+    double sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += distr[i];
+    }
+
+    const double digamma_sum = digamma(sum);
+    for (int i = 0; i < n; i++) {
+        result[i] = exp(digamma(distr[i]) - digamma_sum);
+    }
+}
+
+void normalize_distribution(int n, double distr[n]) {
+    double sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += distr[i];
+    }
+
+    const double norm = 1 / sum;
+    for (int i = 0; i < n; i++) {
+        distr[i] *= norm;
+    }
 }
