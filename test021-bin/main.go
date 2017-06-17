619bc4e1aaba56713db5a23c44c8aa0dcc04f75f
frickiericker <frickiericker@gmail.com>
2017-06-18 06:20:09 +0900

    test021: Binning

--- test021-bin/main.go
+++ test021-bin/main.go
@@ -16,26 +16,49 @@ func main() {
 
 func run() error {
 
-	binSize := 15
-	winSize := 20
-	unitSize := gcd(binSize, winSize)
-	fmt.Println(unitSize)
+	binSize := int64(8)
 
 	scanner := bufio.NewScanner(os.Stdin)
 	delimiter := "\t"
 
-	// Data
-	for scanner.Scan() {
-		record := strings.Split(scanner.Text(), delimiter)
+	curEnd := binSize
+	curVal := float64(0)
 
-		_, err1 := strconv.ParseInt(record[0], 10, 64)
-		_, err2 := strconv.ParseInt(record[1], 10, 64)
-		_, err3 := strconv.ParseFloat(record[2], 64)
+	output := func(start, end int64, value float64) {
+		fmt.Printf("%d\t%d\t%g\n", start, end, value / float64(end - start))
+	}
+
+	for scanner.Scan() {
+		record := strings.SplitN(scanner.Text(), delimiter, 3)
+
+		start, err1 := strconv.ParseInt(record[0], 10, 64)
+		end, err2 := strconv.ParseInt(record[1], 10, 64)
+		val, err3 := strconv.ParseFloat(record[2], 64)
 
 		if err := firstErr(err1, err2, err3); err != nil {
 			return err
 		}
+
+		// Fill the gap
+		for start >= curEnd {
+			output(curEnd - binSize, curEnd, curVal)
+			curEnd += binSize
+			curVal = 0
+		}
+
+		// Consume input
+		for end > curEnd {
+			curVal += float64(curEnd - start) * val
+			output(curEnd - binSize, curEnd, curVal)
+			start = curEnd
+			curEnd += binSize
+			curVal = 0
+		}
+
+		// Accumulate remaining part
+		curVal += float64(end - start) * val
 	}
+	output(curEnd - binSize, curEnd, curVal)
 
 	return scanner.Err()
 }
@@ -47,11 +70,4 @@ func firstErr(errs ...error) error {
 		}
 	}
 	return nil
-}
-
-func gcd(n, m int) int {
-	for n > 0 {
-		m, n = n, m%n
-	}
-	return m
 }
