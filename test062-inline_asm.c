104b43f72c816089bfae6381b5eaee6ee5ed3d60
frickiericker <frickiericker@gmail.com>
2017-09-23 22:34:01 +0900

    Add test062: Inline assembly with MS syntax

---
+++ test062-inline_asm.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int greatest_common_divisor(int m, int n)
+{
+    int result;
+
+    __asm { // -fms-extensions
+        mov  esi, m
+        mov  edi, n
+
+        cmp  esi, edi
+        jge  L_go
+        mov  ecx, esi
+        mov  esi, edi
+        mov  edi, ecx
+
+      L_go:
+        test edi, edi
+        jz   L_done
+
+        xor  edx, edx
+        mov  eax, esi
+        div  edi
+        mov  esi, edi
+        mov  edi, edx
+
+        jmp  L_go
+
+      L_done:
+        mov  result, esi
+    }
+
+    return result;
+}
+
+int main()
+{
+    printf("%d\n", greatest_common_divisor(1029, 1071));
+    return 0;
+}
