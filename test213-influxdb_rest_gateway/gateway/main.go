e854b34ab0f778a99de90457181552112512278c
snsinfu <snsinfu@gmail.com>
2018-01-14 02:03:37 +0900

    test213: Get rid of the global

--- test213-influxdb_rest_gateway/gateway/main.go
+++ test213-influxdb_rest_gateway/gateway/main.go
@@ -10,27 +10,36 @@ import (
 	"github.com/labstack/echo/middleware"
 )
 
-var (
-	db client.Client
-)
-
 func main() {
-	db = newClient()
 	e := echo.New()
 
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
+	e.Use(bind("influx", newClient()))
 
 	e.GET("/ping", handlePing)
 
 	e.Logger.Fatal(e.Start(os.Getenv("GATEWAY_ADDRESS")))
 }
 
+func bind(key string, value interface{}) echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			c.Set(key, value)
+			return next(c)
+		}
+	}
+}
+
 func handlePing(c echo.Context) error {
-	latency, msg, err := db.Ping(10 * time.Second)
+
+	influx := c.Get("influx").(client.Client)
+
+	latency, msg, err := influx.Ping(10 * time.Second)
 	if err != nil {
 		return err
 	}
+
 	return c.JSON(http.StatusOK, map[string]interface{}{
 		"latency": float64(latency) / float64(time.Second),
 		"message": msg,
