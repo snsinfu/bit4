231dddb4ebd175e527b1fcd2c436fb8ed0dcd9a1
snsinfu <snsinfu@gmail.com>
2018-01-14 03:14:44 +0900

    test213: Add /temperature handler

--- test213-influxdb_rest_gateway/gateway/controller.go
+++ test213-influxdb_rest_gateway/gateway/controller.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"net/http"
+	"os"
+	"strconv"
 	"time"
 
 	"github.com/influxdata/influxdb/client/v2"
@@ -20,4 +23,50 @@ func handlePing(c echo.Context) error {
 		"latency": float64(latency) / float64(time.Second),
 		"message": msg,
 	})
+}
+
+func handleTemperaturePost(c echo.Context) error {
+	batch, err := client.NewBatchPoints(client.BatchPointsConfig{
+		Database:  os.Getenv("GATEWAY_INFLUXDB_DATABASE"),
+		Precision: "ms",
+	})
+	if err != nil {
+		return err
+	}
+
+	tags := map[string]string{
+		"via": "gateway",
+	}
+	fields := map[string]interface{}{
+		"temperature": 0.0,
+	}
+	timestamp := time.Now()
+
+	scanner := bufio.NewScanner(c.Request().Body)
+	for scanner.Scan() {
+		value, err := strconv.ParseFloat(scanner.Text(), 64)
+		if err != nil {
+			return err
+		}
+
+		fields["temperature"] = value
+
+		pt, err := client.NewPoint("metrics", tags, fields, timestamp)
+		if err != nil {
+			return err
+		}
+		batch.AddPoint(pt)
+
+		timestamp = timestamp.Add(time.Millisecond)
+	}
+	if err := scanner.Err(); err != nil {
+		return err
+	}
+
+	influx := c.Get("influx").(client.Client)
+	if err := influx.Write(batch); err != nil {
+		return err
+	}
+
+	return c.NoContent(http.StatusOK)
 }
