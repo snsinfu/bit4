c7f9969ad4c8f2b72e6df85963474d358a1a4b82
snsinfu <snsinfu@gmail.com>
2018-01-14 01:54:12 +0900

    test213: Add echo server

--- test213-influxdb_rest_gateway/gateway/main.go
+++ test213-influxdb_rest_gateway/gateway/main.go
@@ -1,9 +1,38 @@
 package main
 
 import (
-	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/influxdata/influxdb/client/v2"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+var (
+	db client.Client
 )
 
 func main() {
-	c := newClient()
+	db = newClient()
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/ping", handlePing)
+
+	e.Logger.Fatal(e.Start(os.Getenv("GATEWAY_ADDRESS")))
+}
+
+func handlePing(c echo.Context) error {
+	latency, msg, err := db.Ping(10 * time.Second)
+	if err != nil {
+		return err
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"latency": float64(latency) / float64(time.Second),
+		"message": msg,
+	})
 }
