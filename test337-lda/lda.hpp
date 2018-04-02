c7ce0d0ec5ac6ac08bda62cccb6ff6e657f081d5
snsinfu <snsinfu@gmail.com>
2018-04-02 15:53:09 +0900

    test337: Move LDA class into a header file

---
+++ test337-lda/lda.hpp
@@ -0,0 +1,148 @@
+#ifndef INCLUDED_LDA_HPP
+#define INCLUDED_LDA_HPP
+
+#include <cmath>
+#include <cstddef>
+
+#include <xtensor/xmath.hpp>
+#include <xtensor/xtensor.hpp>
+
+#include "digamma.hpp"  // digamma
+#include "index.hpp"    // ij, ijk
+
+
+class latent_dirichlet_allocation
+{
+    static inline constexpr double epsilon()
+    {
+        return 1e-6;
+    }
+
+  public:
+    struct config
+    {
+        std::size_t topic_count = 2;
+        double doc_topic_prior = 1;
+        double topic_word_prior = 1;
+        int m_step_count = 10;
+        int e_step_count = 10;
+        int evaluation_interval = 5;
+    };
+
+    explicit latent_dirichlet_allocation(config cfg)
+        : topic_count_{cfg.topic_count}
+        , doc_topic_prior_{cfg.doc_topic_prior}
+        , topic_word_prior_{cfg.topic_word_prior}
+        , m_step_count_{cfg.m_step_count}
+        , e_step_count_{cfg.e_step_count}
+        , evaluation_interval_{cfg.evaluation_interval}
+    {
+    }
+
+    void fit(xt::xtensor<double, 2> const& data)
+    {
+        auto const data_shape = data.shape();
+        auto const doc_count = data_shape[0];
+        auto const word_count = data_shape[1];
+
+        doc_topic_dirichlets_.resize({doc_count, topic_count_});
+        topic_word_dirichlets_.resize({topic_count_, word_count});
+        doc_word_topic_distr_.resize({doc_count, word_count, topic_count_});
+
+        xt::xtensor<double, 2> doc_topic_geoexp(doc_topic_dirichlets_.shape());
+        xt::xtensor<double, 2> topic_word_geoexp(topic_word_dirichlets_.shape());
+        xt::xtensor<double, 2> doc_word_topic_norm({doc_count, word_count});
+
+        topic_word_dirichlets_ = 0.5 + xt::random::rand<double>(topic_word_dirichlets_.shape());
+
+        for (int m_step = 0; m_step < m_step_count_; ++m_step) {
+            topic_word_geoexp = xt::exp(digamma(topic_word_dirichlets_)
+                                - ij::i(digamma(xt::sum(topic_word_dirichlets_, {1}))));
+
+            doc_topic_dirichlets_ = 0.5 + xt::random::rand<double>(doc_topic_dirichlets_.shape());
+
+            for (int e_step = 0; e_step < e_step_count_; ++e_step) {
+                doc_topic_geoexp = xt::exp(digamma(doc_topic_dirichlets_)
+                                   - ij::i(digamma(xt::sum(doc_topic_dirichlets_, {1}))));
+
+                doc_word_topic_distr_ = ijk::ik(doc_topic_geoexp) * ijk::kj(topic_word_geoexp);
+                doc_word_topic_norm = 1 / (xt::sum(doc_word_topic_distr_, {2}) + epsilon());
+                doc_word_topic_distr_ *= ijk::ij(doc_word_topic_norm);
+
+                doc_topic_dirichlets_ = doc_topic_prior_ + xt::sum(ijk::ij(data) * doc_word_topic_distr_, {1});
+
+                if (e_step % evaluation_interval_ == 0) {
+                    // TODO
+                    (void) estimate_log_likelihood(data);
+                }
+            }
+
+            // Workaround: Direct application of xt::transpose() fails.
+            xt::xtensor<double, 2> const transposed_topic_word_dirichlets =
+                topic_word_prior_ + xt::sum(ijk::ij(data) * doc_word_topic_distr_, {0});
+            topic_word_dirichlets_ = xt::transpose(transposed_topic_word_dirichlets);
+        }
+    }
+
+    double estimate_log_likelihood(xt::xtensor<double, 2> const& data) const
+    {
+        // FIXME: Mess.
+
+        auto const word_count = data.shape()[1];
+
+        xt::xtensor<double, 2> dt = digamma(doc_topic_dirichlets_)
+                                    - ij::i(digamma(xt::sum(doc_topic_dirichlets_, {1})));
+
+        xt::xtensor<double, 2> tw = digamma(topic_word_dirichlets_)
+                                    - ij::i(digamma(xt::sum(topic_word_dirichlets_, {1})));
+
+        xt::xtensor<double, 3> dwt = ijk::ik(dt) + ijk::kj(tw)
+                                     - xt::log(doc_word_topic_distr_ + epsilon());
+
+        auto const& L_dt = xt::sum((doc_topic_prior_ - doc_topic_dirichlets_) * dt);
+        auto const& L_tw = xt::sum((topic_word_prior_ - topic_word_dirichlets_) * tw);
+        auto const& L_dwt = xt::sum(ijk::ij(data) * doc_word_topic_distr_ * dwt);
+
+        xt::xtensor<double, 1> lbdt = xt::sum(xt::lgamma(doc_topic_dirichlets_), {1})
+                                      - xt::lgamma(xt::sum(doc_topic_dirichlets_, {1}));
+
+        xt::xtensor<double, 1> lbtw = xt::sum(xt::lgamma(topic_word_dirichlets_), {1})
+                                      - xt::lgamma(xt::sum(topic_word_dirichlets_, {1}));
+
+        double const dt_prior = topic_count_ * std::lgamma(doc_topic_prior_)
+                                - std::lgamma(topic_count_ * doc_topic_prior_);
+
+        double const tw_prior = word_count * std::lgamma(topic_word_prior_)
+                                - std::lgamma(word_count * topic_word_prior_);
+
+        auto const& L_lbdt = xt::sum(lbdt + dt_prior);
+        auto const& L_lbtw = xt::sum(lbtw + tw_prior);
+
+        return (L_dt + L_lbdt + L_tw + L_lbtw + L_dwt)();
+    }
+
+    xt::xtensor<double, 2> doc_topic_distribution() const
+    {
+        return doc_topic_dirichlets_ / ij::i(xt::sum(doc_topic_dirichlets_, {1}));
+    }
+
+    xt::xtensor<double, 2> topic_word_distribution() const
+    {
+        return topic_word_dirichlets_ / ij::i(xt::sum(topic_word_dirichlets_, {1}));
+    }
+
+  private:
+    std::size_t topic_count_;
+    double doc_topic_prior_;
+    double topic_word_prior_;
+
+    int m_step_count_;
+    int e_step_count_;
+    int evaluation_interval_;
+
+    xt::xtensor<double, 2> doc_topic_dirichlets_;
+    xt::xtensor<double, 2> topic_word_dirichlets_;
+    xt::xtensor<double, 3> doc_word_topic_distr_;
+};
+
+#endif
