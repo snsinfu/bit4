6d9e8a5601e5db21538eb21bbb80034c56b09ad7
snsinfu <snsinfu@gmail.com>
2018-04-07 22:26:51 +0900

    test356: Make reproduction minimal

--- test356-xtensor_function/main.cc
+++ test356-xtensor_function/main.cc
@@ -17,17 +17,11 @@ auto log_beta(E&& e)
     return xt::sum(xt::lgamma(e), {1}) - xt::lgamma(xt::sum(e, {1}));
 }
 
-template<typename E>
-auto beta(E&& e)
-{
-    return xt::exp(log_beta(e));
-}
-
 int main()
 {
     xt::xtensor<double, 2> X({10, 5});
     X = 1 + xt::random::rand<double>(X.shape());
-    auto Y = beta(X + X);
+    auto Y = log_beta(X + X);
 
     std::cout << Y << '\n';
 }
