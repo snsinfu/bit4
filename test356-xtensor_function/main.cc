3782e8027bc69fb075cd632f6bc56e000be87a00
snsinfu <snsinfu@gmail.com>
2018-04-07 21:58:06 +0900

    test356: Trigger memory error
    Let's see how this memory error can be fixed.

--- test356-xtensor_function/main.cc
+++ test356-xtensor_function/main.cc
@@ -11,13 +11,23 @@
 #include "beta.hpp"
 
 
+template<typename E>
+auto log_beta(E&& e)
+{
+    return xt::sum(xt::lgamma(e), {1}) - xt::lgamma(xt::sum(e, {1}));
+}
+
+template<typename E>
+auto beta(E&& e)
+{
+    return xt::exp(log_beta(e));
+}
+
 int main()
 {
     xt::xtensor<double, 2> X({10, 5});
+    X = 1 + xt::random::rand<double>(X.shape());
+    auto Y = beta(X + X);
 
-    X = xt::random::rand<double>(X.shape());
-
-    auto log_beta = xt::sum(xt::lgamma(X), {1}) - xt::lgamma(xt::sum(X, {1}))
-
-    std::cout << log_beta << '\n';
+    std::cout << Y << '\n';
 }
