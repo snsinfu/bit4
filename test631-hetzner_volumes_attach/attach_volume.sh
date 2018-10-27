607723a8ee658197f808e679e46205ecf582c308
snsinfu <snsinfu@gmail.com>
2018-10-28 04:32:38 +0900

    test631: Fix attach_volume.sh argument handling

--- test631-hetzner_volumes_attach/attach_volume.sh
+++ test631-hetzner_volumes_attach/attach_volume.sh
@@ -1,8 +1,11 @@
 #!/bin/sh -eu
 
+server_id="$1"
+volume_id="$2"
+
 curl -fsS \
      -X POST \
      -H "Authorization: Bearer ${HCLOUD_TOKEN}" \
      -H "Content-Type: application/json" \
-     -d "{\"server\": $(terraform output server)}" \
-     https://api.hetzner.cloud/v1/volumes/$(terraform output volume)/actions/attach
+     -d "{\"server\": ${server_id}}" \
+     https://api.hetzner.cloud/v1/volumes/${volume_id}/actions/attach
