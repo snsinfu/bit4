47473654f113cf88e9a73f8c5762abd831d1302f
snsinfu <snsinfu@gmail.com>
2018-11-03 02:30:13 +0900

    test643: array_erasure

--- test643-type_erased_array/main.cc
+++ test643-type_erased_array/main.cc
@@ -1,43 +1,91 @@
+#include <cassert>
 #include <cstddef>
+#include <memory>
 #include <vector>
 
 
 namespace
 {
-    class boxed_array
+    class array_erasure
     {
+    private:
+        array_erasure() = default;
+
     public:
-        virtual ~boxed_array() = default;
-        virtual std::size_t size() const = 0;
-        virtual void resize(std::size_t n) = 0;
+        virtual ~array_erasure() = default;
+
+        // size returns the length of the array.
+        virtual std::size_t size() = 0;
+
+        // resize extends or shrinks the array to given length.
+        virtual void resize(std::size_t size) = 0;
+
+        // instance is the only allowed implementation of array_erasure.
+        template<typename T>
+        class instance;
+
+        // make returns a unique_ptr of newly constructed instance<T>.
+        template<typename T>
+        static std::unique_ptr<instance<T>> make(std::size_t size, T def)
+        {
+            return std::unique_ptr<instance<T>>{new instance<T>(size, def)};
+        }
+
+        // recover returns a reference to the erased instance<T>. Assertion
+        // fails if T is not the erased element type.
+        template<typename T>
+        instance<T>& recover()
+        {
+            assert(dynamic_cast<instance<T>*>(this) != nullptr);
+            return static_cast<instance<T>&>(*this);
+        }
     };
 
     template<typename T>
-    class boxed_array_erasure : public boxed_array
+    class array_erasure::instance : public array_erasure
     {
     public:
-        boxed_array_erasure(std::size_t n, T init)
-            : values_(n), init_{init}
+        instance(std::size_t size, T def)
+            : values_(size), default_{def}
         {
         }
 
-        std::size_t size() const override
+        // data returns the pointer to the first element of the array.
+        T* data()
+        {
+            return values_.data();
+        }
+
+        // size returns the length of the array.
+        std::size_t size() override
         {
             return values_.size();
         }
 
-        void resize(std::size_t n) override
+        // resize extends or shrinks the array to given length. New elements
+        // are initialized with the default value.
+        void resize(std::size_t size) override
         {
-            values_.resize(n, init_);
+            values_.resize(size, default_);
         }
 
     private:
         std::vector<T> values_;
-        T init_;
+        T default_;
     };
 }
 
 
+#include <iostream>
+#include <memory>
+
+
 int main()
 {
+    std::unique_ptr<array_erasure> array{array_erasure::make<double>(10, 1.23)};
+
+    array->resize(50);
+
+    std::cout << array->recover<double>().data() << '\n';
+    std::cout << array->recover<double>().size() << '\n';
 }
