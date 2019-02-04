d176111c9af02e6f542893807bfacad29a0e9f16
snsinfu <snsinfu@gmail.com>
2019-02-04 16:53:23 +0900

    test719: Implement actual neighbor search

--- test719-periodic_system_neighbor_search/main.cc
+++ test719-periodic_system_neighbor_search/main.cc
@@ -56,7 +56,7 @@ int main()
     y_meansq /= n_points;
 
     const auto y_stddev = std::sqrt(y_meansq - y_mean * y_mean);
-    const auto est_y_span = 3 * y_stddev;
+    const auto est_y_span = 3 * y_stddev; // Not optimal
     const auto est_density = n_points / (x_span * est_y_span);
 
     // Define the hash table parameters
@@ -83,11 +83,7 @@ int main()
     std::clog << "x_bins: " << x_bins << '\n';
     std::clog << "y_bins: " << y_bins << '\n';
 
-    auto const hash_coords = [&](uint32_t x, uint32_t y) {
-        return x + x_bins * (y % y_bins);
-    };
-
-    // Assign points to the hash table
+    // Create hash table
 
     struct spatial_bucket
     {
@@ -97,9 +93,43 @@ int main()
             uint32_t index;
         };
         std::vector<member> members;
+        std::vector<uint32_t> neighbors;
     };
 
     std::vector<spatial_bucket> buckets(n_buckets);
+
+    for (uint32_t x = 0; x < x_bins; x++) {
+        for (uint32_t y_hash = 0; y_hash < y_bins; y_hash++) {
+            const auto bucket_index = x + x_bins * y_hash;
+            auto& bucket = buckets[bucket_index];
+
+            for (const uint32_t dx : {0u, 1u, x_bins - 1}) {
+                for (const uint32_t dy_hash : {0u, 1u, y_bins - 1}) {
+                    const auto neighbor_index =
+                        (x + dx) % x_bins + x_bins * ((y_hash + dy_hash) % y_bins);
+                    if (neighbor_index <= bucket_index) {
+                        bucket.neighbors.push_back(neighbor_index);
+                    }
+                }
+            }
+
+            std::sort(bucket.neighbors.begin(), bucket.neighbors.end());
+
+            bucket.neighbors.erase(
+                std::unique(
+                    bucket.neighbors.begin(),
+                    bucket.neighbors.end()
+                ),
+                bucket.neighbors.end()
+            );
+        }
+    }
+
+    // Assign points to the hash table
+
+    auto const hash_coords = [&](uint32_t x, uint32_t y) {
+        return x + x_bins * (y % y_bins);
+    };
 
     for (uint32_t index = 0; index < n_points; index++) {
         const auto point = points[index];
@@ -133,4 +163,55 @@ int main()
     }
     mean_occupancy /= n_buckets;
     std::clog << "\nmean_occupancy: " << mean_occupancy << '\n';
+
+    // Neighbor search
+
+    const auto nearest_displacement = [&](const vec& p1, const vec& p2) {
+        auto r = p1 - p2;
+        r.x = std::remainder(r.x, x_span);
+        return r;
+    };
+
+    const auto dcut_sq = dcut * dcut;
+    uint32_t collision_hash = 0;
+    uint32_t nops = 0;
+
+    for (const auto& bucket : buckets) {
+        for (const auto neighbor_index : bucket.neighbors) {
+            const auto& neighbor = buckets[neighbor_index];
+
+            for (const auto mi : bucket.members) {
+                for (const auto mj : neighbor.members) {
+                    if (mi.index == mj.index) {
+                        break; // Avoid double counting
+                    }
+
+                    const auto r = nearest_displacement(points[mi.index], points[mj.index]);
+                    if (squared_norm(r) < dcut_sq) {
+                        collision_hash += mi.index ^ mj.index;
+                    }
+                    nops++;
+                }
+            }
+        }
+    }
+
+    std::clog << "colhash (hash table): " << collision_hash << " (" << nops << " ops)\n";
+
+    // Neighbor search
+
+    collision_hash = 0;
+    nops = 0;
+
+    for (uint32_t i = 0; i < n_points; i++) {
+        for (uint32_t j = 0; j < i; j++) {
+            const auto r = nearest_displacement(points[i], points[j]);
+            if (squared_norm(r) < dcut_sq) {
+                collision_hash += i ^ j;
+            }
+            nops++;
+        }
+    }
+
+    std::clog << "colhash (bruteforce): " << collision_hash << " (" << nops << " ops)\n";
 }
