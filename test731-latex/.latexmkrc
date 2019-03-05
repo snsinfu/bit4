17fdf3550bffb9337a641e0aa9e885fe818067f9
snsinfu <snsinfu@gmail.com>
2019-03-05 15:43:50 +0900

    Add test731: latex

---
+++ test731-latex/.latexmkrc
@@ -0,0 +1,7 @@
+$latex        = 'uplatex -synctex=1 -halt-on-error';
+$latex_silent = 'uplatex -synctex=1 -halt-on-error -interaction=batchmode';
+$bibtex       = 'biber';
+$makeindex    = 'mendex %O -o %D %S';
+$dvipdf       = 'dvipdfmx %O -o %D %S';
+$pdf_mode     = 3;
+$out_dir      = 'out';
---
+++ test731-latex/make_figure.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+x = np.linspace(-10, 10, num=1000)
+y = np.sin(x) / (x + 1e-6)
+
+fig = plt.figure(figsize=(5, 3), tight_layout={"rect": (0, 0, 1, 0.95)}, dpi=72)
+
+ax = fig.add_subplot(1, 1, 1)
+ax.plot(x, y)
+
+fig.suptitle("Lorem ipsum")
+fig.savefig("ipsum.pdf")
