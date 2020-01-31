361a8a3fc8ac61c57f554257b2c4aefb0f6e4b2e
snsinfu <snsinfu@gmail.com>
2020-01-31 18:29:03 +0900

    Add test935: Prototyping simple H5 lib

---
+++ test935-simple_h5/main.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <random>
+#include <vector>
+
+#include "h5.hpp"
+
+
+struct vec
+{
+    double x = 0;
+    double y = 0;
+    double z = 0;
+};
+
+
+int main()
+{
+    h5::file file{"_test.h5"};
+
+    std::mt19937_64 random;
+    std::vector<vec> points;
+    for (int i = 0; i < 1000; i++) {
+        std::normal_distribution<double> normal;
+        points.push_back({normal(random), normal(random), normal(random)});
+    }
+
+    auto data = file.dataset<float, 2>("/data/points");
+    //data.write(
+    //    reinterpret_cast<double const*>(points.data()),
+    //    {points.size(), 3},
+    //    {.compression = 5, .scaleoffset = 3}
+    //);
+}
