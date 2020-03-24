82d85d00483de55c0a118040fd87642218af5c3c
snsinfu <snsinfu@gmail.com>
2020-03-25 01:24:48 +0900

    test956: Update option handling for next Ansible release (2.10)
    The current feature release (2.9) does not support configurable vars
    plugins. The next feature release is planned on 2020-07-30 [1]. For now
    I'd just use an environment variable directly.
    
    [1]: https://github.com/ansible/ansible/blob/6369591b11a9f42ca9ecc33d6cb194b3ce51cbe4/docs/docsite/rst/roadmap/ROADMAP_2_10.rst

---
+++ test956-ansible_config_vars_plugin/README.md
@@ -0,0 +1,4 @@
+# Ansible: Automatically load and merge variables from files in config directory
+
+- https://docs.ansible.com/ansible/latest/dev_guide/developing_plugins.html#vars-plugins
+- https://github.com/ansible/ansible/commit/c1f1b2029c425665622608001207267e8f2dc176
--- test956-ansible_config_vars_plugin/vars_plugins/config_vars.py
+++ test956-ansible_config_vars_plugin/vars_plugins/config_vars.py
@@ -1,17 +1,23 @@
 import os
 
 from ansible.constants import config
+from ansible.plugins import AnsiblePlugin
 from ansible.plugins.vars import BaseVarsPlugin
 from ansible.utils.vars import merge_hash
 
 
 # TODO: Make dirname customizable via ansible.cfg.
+# -> Current release (2.9) does not have a unified way of configuring vars
+# plugins. That feature will be added in 2.10. Until then, we just reference
+# the environment variable directly.
+# See: https://github.com/ansible/ansible/commit/c1f1b2029c425665622608001207267e8f2dc176
 
 DOCUMENTATION = """
 vars: config_vars
 short_description: Merge variables from files in config directory
 options:
   dirname:
+    type: str
     ini:
       - key: dirname
         section: config_vars
@@ -19,10 +25,20 @@ options:
       - name: ANSIBLE_CONFIG_VARS_DIRNAME
 """
 
+DEFAULT_DIRNAME = "config"
 
-class VarsModule(BaseVarsPlugin):
-    def get_vars(self, loader, path, entities, dirname="config"):
+
+# BaseVarsPlugin does not inherit AnsiblePlugin in < 2.10.
+class VarsModule(AnsiblePlugin, BaseVarsPlugin):
+    def get_vars(self, loader, path, entities, cache=True):
         super().get_vars(loader, path, entities)
+
+        dirname = DEFAULT_DIRNAME
+        try:
+            dirname = self.get_option("dirname")
+        except KeyError:
+            pass
+        dirname = os.getenv("ANSIBLE_CONFIG_VARS_DIRNAME") or dirname
 
         merged_vars = {}
 
