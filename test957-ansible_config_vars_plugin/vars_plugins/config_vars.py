fe903aa5f85825eb1295d8d8a161b234c7eb62a8
snsinfu <snsinfu@gmail.com>
2020-03-30 01:28:06 +0900

    test957: Add caching
    The vars plugin is called multiple times in every task. I/O like listing
    vars files can add up to significant load.

--- test957-ansible_config_vars_plugin/vars_plugins/config_vars.py
+++ test957-ansible_config_vars_plugin/vars_plugins/config_vars.py
@@ -1,10 +1,10 @@
+import functools
 import os
 
 from ansible.plugins.vars import BaseVarsPlugin
 from ansible.utils.vars import merge_hash
 
 
-# Ansible <= 2.9 does not pass options to vars plugins.
 DOCUMENTATION = """
 vars: config_vars
 short_description: Merge variables from files in config directory
@@ -21,13 +21,18 @@ options:
 DEFAULT_PATH = "config"
 
 
+@functools.lru_cache
+def find_vars_files(loader, basedir, path):
+    return loader.find_vars_files(basedir, path)
+
+
 class VarsModule(BaseVarsPlugin):
-    def get_vars(self, loader, path, entities, cache=True):
+    def get_vars(self, loader, path, entities):
         super().get_vars(loader, path, entities)
 
-        # TODO: Use get_option once Ansible 2.10 is released.
+        # TODO: Change to get_option once Ansible 2.10 is released.
         config_path = os.getenv("ANSIBLE_CONFIG_VARS_PATH") or DEFAULT_PATH
-        vars_files = loader.find_vars_files(self._basedir, config_path)
+        vars_files = find_vars_files(loader, self._basedir, config_path)
 
         merged_vars = {}
         for file_path in sorted(vars_files):
