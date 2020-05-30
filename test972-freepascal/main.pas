a9a5d821a4d698563f8829c43cea5f8804fff8cf
snsinfu <snsinfu@gmail.com>
2020-05-30 15:28:15 +0900

    Add test972: Free Pascal

---
+++ test972-freepascal/main.pas
@@ -0,0 +1,10 @@
+{$mode objfpc}{$H+}{$J-}
+
+program main;
+begin
+  WriteLn('Hello world!');
+end.
+
+// See:
+//   https://castle-engine.io/modern_pascal_introduction.html
+//   https://freepascal.org/docs.html
